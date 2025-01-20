e6b767aefbcf17a5d18ad80909108d62398445fe
rhmoult <rhmoult@gmail.com>
2025-01-20 11:03:50 -0500

    Add two methods for comparing possible anagrams

---
+++ arrayandstringbasics/anagrams.py
@@ -0,0 +1,32 @@
+from collections import Counter
+from sys import argv
+
+
+def is_anagram_via_counter(baseline, possible_anagram):
+    baseline_count = Counter(baseline)
+    possible_anagram_count = Counter(possible_anagram)
+    return baseline_count == possible_anagram_count
+
+
+def is_anagram_manually(baseline, possible_anagram):
+    return char_count(baseline) == char_count(possible_anagram)
+
+
+def char_count(myString):
+    count = {}
+
+    for char in myString:
+        if char not in count:
+            count[char] = 0
+
+        count[char] += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    if len(argv) > 2:
+        one = argv[1]
+        two = argv[2]
+        print(is_anagram_via_counter(one, two))
+        print(is_anagram_manually(one, two))
