13e499b4c58b469f45af6205e10a97f73624cca0
rhmoult <rhmoult@gmail.com>
2025-02-05 08:54:25 -0500

    Add computational complexity

--- arrayandstringbasics/anagrams.py
+++ arrayandstringbasics/anagrams.py
@@ -2,16 +2,22 @@ from collections import Counter
 from sys import argv
 
 
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def is_anagram_via_counter(baseline, possible_anagram):
     baseline_count = Counter(baseline)
     possible_anagram_count = Counter(possible_anagram)
     return baseline_count == possible_anagram_count
 
 
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def is_anagram_manually(baseline, possible_anagram):
     return char_count(baseline) == char_count(possible_anagram)
 
 
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def char_count(myString):
     count = {}
 
