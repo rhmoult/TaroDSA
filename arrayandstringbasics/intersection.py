fe1ecf344a42aed875fe4beaa7005b55cd24b348
rhmoult <rhmoult@gmail.com>
2025-02-05 08:56:59 -0500

    Add computational complexity

--- arrayandstringbasics/intersection.py
+++ arrayandstringbasics/intersection.py
@@ -1,6 +1,10 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def intersection(firstArray, secondArray):
     firstArrayAsSet = set(firstArray)
-    return [element for element in secondArray if element in firstArrayAsSet]
+    return [
+        element for element in secondArray if element in firstArrayAsSet
+    ]  # Lookup in set is O(1)
 
 
 if __name__ == "__main__":
